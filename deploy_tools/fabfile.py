f263248b66526f9fb25c6b8b720e3ee46b431990
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-04 23:15:42 -0700

    inserted missing lines

--- deploy_tools/fabfile.py
+++ deploy_tools/fabfile.py
@@ -1,6 +1,7 @@
 import random
 from fabric.contrib.files import append, exists
 from fabric.api import cd, env, local, run
+import os
 REPO_URL = 'https://github.com/oeawFebe/tddWithPython.git'
 
 
@@ -39,6 +40,8 @@ def _create_or_update_dotenv():
         new_secret = "".join(random.SystemRandom().choices(
             'abcdefghijklmnopqrstmvwxyz0123456789', k=50))
         append('.env', f'DJANGO_SECRET_KEY={new_secret}')
+    email_password=os.environ['EMAIL_PASSWORD']
+    append('.env',f'EMAIL_PASSWORD={email_password}')
 
 
 def _update_static_files():
