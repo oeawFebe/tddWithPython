cc30ffdaf609b4185440f6aef21dd82f93fa0d1b
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-17 13:28:35 -0700

    corrected typo

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -15,7 +15,7 @@ def reset_database(host):
         run(f'{manage_dot_py} flush --noinput')
 
 
-def _get_serer_env_vars(host):
+def _get_server_env_vars(host):
     env_lines = run(f'cat ~/sites/{host}/.env').splitlines()
     return dict(l.split("=") for l in env_lines if l)
 
