1934163ee84cb6394c431c3e9406321418c96944
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-17 14:26:05 -0700

    I fixed the problem that I have to type pw once.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -9,9 +9,10 @@ def _get_manage_dot_py(host):
 
 def reset_database(host):
     PW=os.environ.get("PW")#
+    print("printing PW from reset_database",PW)#
     manage_dot_py = _get_manage_dot_py(host)
 
-    with settings(host_string=f'ubuntu@{host}',user='ubuntu',passwd=PW):
+    with settings(host_string=f'ubuntu@{host}',user='ubuntu',password=PW):#
         run(f'{manage_dot_py} flush --noinput')
 
 
@@ -20,11 +21,11 @@ def _get_server_env_vars(host):
     return dict(l.split("=") for l in env_lines if l)
 
 
-def create_session_on_server(host, email):
-    
+def create_session_on_server(host, email):  
     PW=os.environ.get("PW")#
+    print("printing PW from create_session_on_server",PW)#
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@{host}',user='ubuntu',passwd=PW):
+    with settings(host_string=f'ubuntu@{host}',user='ubuntu',password=PW):#
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
