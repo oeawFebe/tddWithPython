ebaa6ef860d5333fbd799545fe41fadcfc8c6f48
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-17 15:38:16 -0700

    url, placeholder view, and first-cut templates for my_lists

--- lists/views.py
+++ lists/views.py
@@ -1,33 +1,35 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
-from lists.forms import ExistingListItemForm,ItemForm
+from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
-from django.views.generic import FormView,CreateView,DetailView
+from django.views.generic import FormView, CreateView, DetailView
 # Create your views here.
 
 
 class HomePageView(FormView):
-    template_name='home.html'
-    form_class=ItemForm
+    template_name = 'home.html'
+    form_class = ItemForm
+
 
 class NewListView(CreateView):
-    template_name='home.html'
-    form_class=ItemForm
+    template_name = 'home.html'
+    form_class = ItemForm
 
-    def form_valid(self,form):
-        list_=List.objects.create()
+    def form_valid(self, form):
+        list_ = List.objects.create()
         form.save(for_list=list_)
         return redirect(list_)
 
+
 class ViewAndAddToList(DetailView):
-    model=List
-    template_name='list.html'
-    form_class=ExistingListItemForm
-    
+    model = List
+    template_name = 'list.html'
+    form_class = ExistingListItemForm
+
     def get_form(self):
-        self.object=self.get_object()
-        return self.form_class(for_list=self.object,data=self.request.POST)
+        self.object = self.get_object()
+        return self.form_class(for_list=self.object, data=self.request.POST)
 
 
 def view_list(request, list_id):
@@ -35,7 +37,7 @@ def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
     form = ExistingListItemForm(for_list=list_)
     if request.method == "POST":
-        form = ExistingListItemForm(for_list=list_,data=request.POST)
+        form = ExistingListItemForm(for_list=list_, data=request.POST)
         if form.is_valid():
             form.save()
             return redirect(list_)
@@ -43,6 +45,7 @@ def view_list(request, list_id):
 
 
 def new_list(request):
+    
     form = ItemForm(data=request.POST)
     if form.is_valid():
         list_ = List.objects.create()
@@ -50,3 +53,8 @@ def new_list(request):
         return redirect(list_)
     else:
         return render(request, 'home.html', {"form": form})
+
+
+def my_lists(request, email):
+
+    return render(request, 'my_lists.html')
