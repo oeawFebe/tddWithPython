f61a8a61c7af51478a47a9b9dd74f9dc903610bb
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-17 13:15:34 -0700

    Finally I think I can run the functional test to staging server.

--- functional_tests/server_tools.py
+++ functional_tests/server_tools.py
@@ -1,6 +1,8 @@
 from fabric.api import run
 from fabric.context_managers import settings, shell_env
-
+import os
+#import password of ssh
+PW=os.environ.get("PW")
 
 def _get_manage_dot_py(host):
     return f'~/sites/{host}/virtualenv/bin/python3.7 ~/sites/{host}/manage.py'
@@ -9,7 +11,7 @@ def _get_manage_dot_py(host):
 def reset_database(host):
     manage_dot_py = _get_manage_dot_py(host)
 
-    with settings(host_string=f'ubuntu@{host}'):
+    with settings(host_string=f'ubuntu@{host}',user='ubuntu',passwd=PW):
         run(f'{manage_dot_py} flush --noinput')
 
 
@@ -20,7 +22,7 @@ def _get_serer_env_vars(host):
 
 def create_session_on_server(host, email):
     manage_dot_py = _get_manage_dot_py(host)
-    with settings(host_string=f'ubuntu@{host}'):
+    with settings(host_string=f'ubuntu@{host}',user='ubuntu',passwd=PW):
         env_vars = _get_server_env_vars(host)
         with shell_env(**env_vars):
             session_key = run(f'{manage_dot_py} create_session {email}')
