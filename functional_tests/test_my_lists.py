e1f8622b2b9a17dc2c631ca6902e794d071b51b7
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-17 21:05:13 -0700

    add list owners via forms. more isolated tesst

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from .base import FunctionalTest
 from .server_tools import create_session_on_server
 from .management.commands.create_session import create_pre_authenticated_session
+import time
 
 class MyListsTest(FunctionalTest):
     def create_pre_authenticated_session(self,email):
@@ -21,13 +22,12 @@ class MyListsTest(FunctionalTest):
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         # Edith is a logged-in user
         self.create_pre_authenticated_session('edith@example.com')
-        
         #She goes to the home page and starts a list
         self.browser.get(self.live_server_url)
-        self.add_list_item('Reticulate')
+        self.add_list_item('Reticulate splines')
         self.add_list_item('Immanentize eschaton')
         first_list_url=self.browser.current_url
-        
+
         #she notices a "my lists" link, for the first time.
         self.browser.find_element_by_link_text('My lists').click()
 
--- lists/views.py
+++ lists/views.py
@@ -1,34 +1,35 @@
 from django.shortcuts import redirect, render
 from django.core.exceptions import ValidationError
 from django.core.urlresolvers import reverse
-from lists.forms import ExistingListItemForm, ItemForm
 from lists.models import Item, List
 from django.views.generic import FormView, CreateView, DetailView
 from django.contrib.auth import get_user_model
+from lists.forms import ExistingListItemForm,ItemForm,NewListForm
+
 User=get_user_model()
 class HomePageView(FormView):
     template_name = 'home.html'
     form_class = ItemForm
 
 
-class NewListView(CreateView):
-    template_name = 'home.html'
-    form_class = ItemForm
+# class NewListView(CreateView):
+#     template_name = 'home.html'
+#     form_class = ItemForm
 
-    def form_valid(self, form):
-        list_ = List.objects.create()
-        form.save(for_list=list_)
-        return redirect(list_)
+#     def form_valid(self, form):
+#         list_ = List.objects.create()
+#         form.save(for_list=list_)
+#         return redirect(list_)
 
 
-class ViewAndAddToList(DetailView):
-    model = List
-    template_name = 'list.html'
-    form_class = ExistingListItemForm
+# class ViewAndAddToList(DetailView):
+#     model = List
+#     template_name = 'list.html'
+#     form_class = ExistingListItemForm
 
-    def get_form(self):
-        self.object = self.get_object()
-        return self.form_class(for_list=self.object, data=self.request.POST)
+#     def get_form(self):
+#         self.object = self.get_object()
+#         return self.form_class(for_list=self.object, data=self.request.POST)
 
 
 def view_list(request, list_id):
@@ -43,19 +44,25 @@ def view_list(request, list_id):
     return render(request, 'list.html', {'list': list_, 'form': form})
 
 
+# def new_list(request):###old one before switched
+
+#     form = ItemForm(data=request.POST)
+#     if form.is_valid():
+#         list_ = List()
+#         if request.user.is_authenticated:
+#             list_.owner=request.user
+#         list_.save()
+#         form.save(for_list=list_)
+#         return redirect(list_)
+#     else:
+#         return render(request, 'home.html', {"form": form})
+
 def new_list(request):
-
-    form = ItemForm(data=request.POST)
+    form=NewListForm(data=request.POST)
     if form.is_valid():
-        list_ = List()
-        list_.owner=request.user
-        list_.save()
-        form.save(for_list=list_)
+        list_=form.save(owner=request.user)
         return redirect(list_)
-    else:
-        return render(request, 'home.html', {"form": form})
-
-
+    return render(request,'home.html',{'form':form})
 def my_lists(request, email):
     owner=User.objects.get(email=email)
 
