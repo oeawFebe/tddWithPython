82151c9e3aac5f8cfeccc517004cdd0d06f3ae94
oeawFebe <59498011+oeawFebe@users.noreply.github.com>
2020-04-18 22:22:32 -0700

    modified typo in listjs

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,6 +1,6 @@
 window.Superlists={};
 window.Superlists.initialize=function(){
-	$('input[name="text').on('keypress',function (){
+	$('input[name="text"]').on('keypress',function (){
         $('.has-error').hide();
     });
 };
